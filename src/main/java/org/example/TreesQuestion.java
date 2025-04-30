9fa4773ae667543e318a79129921efb2b0c464da
somya3819 <somyamahajan597@gmail.com>
2025-04-30 17:47:03 +0530

    error fix

--- src/main/java/org/example/TreesQuestion.java
+++ src/main/java/org/example/TreesQuestion.java
@@ -1,4 +1,28 @@
 package org.example;
-
+import java.util.*;
+class Node{
+    int val;
+    Node left;
+    Node right;
+    public Node(){left=null;
+        right=null;}
+    public Node(int val){this.val=val;
+        left=null;
+        right=null;}
+}
 public class TreesQuestion {
+    public static void inorder(Node root){
+        if(root==null) return;
+        inorder(root.left);
+        System.out.println(root.val);
+        inorder(root.right);
+    }
+    public static void main(String[] args){
+        Node A=new Node(1);
+        Node B=new Node(2);
+        Node C=new Node(3);
+        C.left=A;
+        C.right=B;
+        inorder(C);
+    }
 }
