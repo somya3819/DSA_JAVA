9107371f49668761ab03be7203f99ba05c3c34ba
somya3819 <somyamahajan597@gmail.com>
2025-07-07 17:27:28 +0530

    error fix!

---
+++ classRoom/src/main/java/org/example/OneParameterLambda.java
@@ -0,0 +1,11 @@
+package org.example;
+
+interface OneParameter{
+    void display(String str);
+}
+public class OneParameterLambda{
+    public static void main(String[] args){
+        OneParameter op=(name)->System.out.println("Hello " + name);
+        op.display("Somya");
+    }
+}
