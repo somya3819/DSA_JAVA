f2d1be146588123cae4d80bdd071d72ad7ae8cdf
somya3819 <somyamahajan597@gmail.com>
2025-07-08 17:31:21 +0530

    error fix!2

--- classRoom/src/main/java/org/example/LambdaCollections.java
+++ classRoom/src/main/java/org/example/LambdaCollections.java
@@ -4,6 +4,12 @@ public class LambdaCollections {
     public static void main(String[] args) {
 //        todo auto-generated method stub
         List<String> names=new ArrayList<>(Arrays.asList("Python", "Java","C++"));
+
+        //printing
         names.forEach(name-> System.out.println(name));
+
+        //upper case
+        names.forEach(name-> System.out.println(name.toUpperCase()));
+
     }
 }
