05a8df4cf52303b801184c08976553379d22284e
somya3819 <somyamahajan597@gmail.com>
2025-07-21 16:52:51 +0530

    21.07.2025

---
+++ classRoom/src/main/java/org/example/ArrayListExample.java
@@ -0,0 +1,16 @@
+package org.example;
+import java.util.*;
+public class ArrayListExample {
+    public static void main(String[] args) {
+        ArrayList<String> al=new ArrayList<>();
+        al.add("Peter -> ");
+        al.add("loves -> ");
+        al.add("Somya");
+        al.add(" hehe ");
+        System.out.println(al);
+        System.out.println("size of arraylist is "+al.size());
+
+        System.out.println(al.contains("Somya"));//return true - "contains method" checks if that particular object is there in the arraylist or not
+        System.out.println(al.contains("atharva")); //return false
+    }
+}
